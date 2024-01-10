c802db3f2a531fbe93f525b2fb91ca4e8d2fabaa
tomohiro ueno <hagurono9@gmail.com>
2024-01-10 19:22:20 +0900

    modelを追加

---
+++ lib/toilet.dart
@@ -0,0 +1,13 @@
+import 'package:google_place/google_place.dart';
+
+class Toilet {
+  String? name;
+  String? photo;
+  Location? location;
+
+  Toilet({
+    this.name,
+    this.photo,
+    this.location,
+  });
+}
